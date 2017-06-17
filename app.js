c4eb20d2a23f4390c7e61fc1accdd4d09e0ae0e9
Jordan Benner <jordanbenner@outlook.com>
2017-06-17 14:48:51 -0500

    fixes#6

--- app.js
+++ app.js
@@ -37,3 +37,7 @@ app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+
+chargebee.configure({site : "<<site_name>>",
+  api_key : "<<api_key>>"
+};
